e9ba53191f5b2cb8c54ba2c16a4860caac15a413
Kama020 <czyzkamila@gmail.com>
2021-05-17 21:32:04 +0200

    first test

--- Cipher.Unittests/UnitTest.cs
+++ Cipher.Unittests/UnitTest.cs
@@ -6,6 +6,11 @@ namespace Calculator.Unittests
     [TestClass]
     public class SubTests
     {
-       
+        [TestMethod]
+        public void Calc_Sub_PP()
+        {
+            Calc.Calculator calc = new Calc.Calculator(2, 1);
+            Assert.AreEqual(calc.Subs(), 1);
+        }
     }
 }
